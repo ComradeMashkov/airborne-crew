26deb199577f4404866263be05324b3e85586857
Илья Машков <comrademashkov@gmail.com>
2023-11-17 11:28:52 +0300

    Added documentation (comments) for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,32 +8,41 @@ using namespace gui_wrapper;
 using namespace objects;
 
 int main(int argc, char* argv[]) {
+    // Создаем и инициализируем окно размером 800х600 и заголовком
     sf::RenderWindow window{ {800, 600}, "Dispatch window" };
     tgui::Gui gui{ window };
-    // window.setFramerateLimit(60);
+    // window.setFramerateLimit(60); - ограничитель кадров
 
+    // Создаем логгер, выводящий все в файл (папка logs)
     log_handler::LogHandler logger("../logs/sample.log");
     event_handler::EventHandler::SetLogger(&logger);
 
+    // Создаем объект самолета
     Plane plane;
 
+    // Создаем координатную метку
     CoordsLabel coords_label;
     coords_label.InitializeCoordsLabel();
 
+    // Создаем метку кадров в секунду
     FrameRateLabel frame_rate_label;
     frame_rate_label.InitializeFrameRateLabel();
 
+    // Создаем полотно
     Canvas canvas;
     canvas.InitializeCanvas(plane);
 
+    // Создаем верхнее меню
     UpperMenu menu;
     menu.InitializeMenu(gui, plane, frame_rate_label);
 
+    // Добавляем все объекты в наш интерфейс
     gui.add(menu.GetMenu());
     gui.add(canvas.GetCanvas());
     gui.add(coords_label.GetLabel());
     gui.add(frame_rate_label.GetLabel());
 
+    // ОСНОВНОЙ ПРОГРАММНЫЙ ЦИКЛ
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
