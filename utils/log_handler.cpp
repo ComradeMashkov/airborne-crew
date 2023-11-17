a4057de3e16d6b086e984d33121c67eb21a2d08a
Илья Машков <comrademashkov@gmail.com>
2023-11-17 11:18:32 +0300

    Deleted log_handler.cpp

--- utils/log_handler.cpp
+++
@@ -1,9 +0,0 @@
-#include "log_handler.h"
-
-namespace log_handler {
-
-// void LogHandler::LogTrivial(SeverityLevel lvl, const std::string& message) {
-//     BOOST_LOG_SEV(logger_, lvl) << message;
-// }
-
-} // namespace log_handler
