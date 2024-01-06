634f7c276db374e92322b23ff18b27f214492e52
Илья Машков <comrademashkov@gmail.com>
2024-01-06 17:18:06 +0300

    Plane class code refactoring

--- src/objects/plane.cpp
+++ src/objects/plane.cpp
@@ -1,12 +1,6 @@
 #include "plane.h"
 
 namespace objects {
-  /*  namespace global_parameters{
-        extern float MAP_TOP_COORDINATES;
-        extern float MAP_LEFT_COORDINATES;
-        extern float MAP_WIDTH;
-        extern float MAP_HEIGHT;
-    }*/
 
 void Plane::SetPrimitive(const sf::Sprite& circle) {
     plane_ = circle;
@@ -14,11 +8,25 @@ void Plane::SetPrimitive(const sf::Sprite& circle) {
 
 void Plane::SetToDraw(bool to_draw) {
     to_draw_ = to_draw;
+    longtitude = "0°";
+    latitude = "0°";
 }
 
 void Plane::SetTargetPosition(const sf::Vector2f& target_position) {
     target_position_ = target_position;
-    tracking_target = true;
+    tracking_target_ = true;
+}
+
+void Plane::SetAngle(float angle) {
+    angle_ = angle;
+}
+
+void Plane::SetLinearSpeed(float linear_speed) {
+    speed_ = linear_speed;
+}
+
+void Plane::SetAngleSpeed(float angle_speed) {
+    angle_speed_ = angle_speed;
 }
 
 sf::Sprite Plane::GetPrimitive() const {
@@ -48,31 +56,64 @@ sf::Vector2f Plane::GetPlaneSize() const {
 }
 
 void Plane::Control() {
-    if (to_draw_){
-        coordinates = "(" + std::to_string(plane_.getPosition().x/480*0.61268+-77.347776) +";" + std::to_string(plane_.getPosition().y/360*-0.36113+39.089530) + ")";
-        sf::Vector2f direction;
-    if (this->tracking_target){
-        direction = target_position_ - plane_.getPosition();
-        if (direction.x > 0)target_angle = asin(direction.y/sqrt(pow(direction.x,2)+pow(direction.y,2)));
-        else target_angle = M_PI-asin(direction.y/sqrt(pow(direction.x,2)+pow(direction.y,2)));
-    }
-
-    // Проверяем, не приблизились ли мы достаточно к цели, чтобы завершить перемещение
-    //if (std::sqrt(direction.x * direction.x + direction.y * direction.y) < 10*speed_) this->tracking_target = false;
-        if(target_angle - angle > 2*M_PI) angle += 2*M_PI;
-        if(angle - target_angle > 2*M_PI) angle -= 2*M_PI;
-        if (angle - target_angle > 0.02f || angle - target_angle < -0.02f){
-            if((target_angle - angle > 0 && target_angle - angle < M_PI) || (target_angle - angle < -M_PI )) angle += angle_speed;
-            else angle -= angle_speed;
-        }
-        else tracking_target = false;
+    if (to_draw_) {
+        double lng = plane_.getPosition().y / global_parameters::PLANE_ANGLE_ROTATION_Y * global_parameters::PLANE_ROTATION_COEFFICIENT_Y + global_parameters::PLANE_INITIAL_ANGLE_POS_Y;
+        double lat = plane_.getPosition().x / global_parameters::PLANE_ANGLE_ROTATION_X * global_parameters::PLANE_ROTATION_COEFFICIENT_X + global_parameters::PLANE_INITIAL_ANGLE_POS_X;
         
+        longtitude = std::to_string(lng) + "°";
+        latitude = std::to_string(lat) + "°";
 
-        sf::Vector2f movement = speed_ * sf::Vector2f(cos(angle),sin(angle));
+        sf::Vector2f direction;
+
+        if (tracking_target_) {
+            direction = target_position_ - plane_.getPosition();
+            if (direction.x > 0) {
+                target_angle_ = asin(direction.y / sqrt(pow(direction.x, 2) + pow(direction.y, 2)));
+            }
+            else {
+                target_angle_ = M_PI - asin(direction.y / sqrt(pow(direction.x, 2) + pow(direction.y, 2)));
+            }
+        }
+
+        if (target_angle_ - angle_ > 2 * M_PI) {
+            angle_ += 2 * M_PI;
+        }
+
+        if (angle_ - target_angle_ > 2 * M_PI) {
+            angle_ -= 2 * M_PI;
+        }
+
+        if (angle_ - target_angle_ > global_parameters::PLANE_CRITICAL_ANGLE || angle_ - target_angle_ < -global_parameters::PLANE_CRITICAL_ANGLE){
+            if ((target_angle_ - angle_ > 0 && target_angle_ - angle_ < M_PI) || (target_angle_ - angle_ < -M_PI )) {
+                angle_ += angle_speed_;
+            }
+            else {
+                angle_ -= angle_speed_;
+            }
+        }
+        else {
+            tracking_target_ = false;
+        }
+
+        sf::Vector2f movement = speed_ * sf::Vector2f(cos(angle_), sin(angle_));
 
         plane_.move(movement);
-        plane_.setRotation(angle/M_PI*180+90);
+        plane_.setRotation(angle_ / M_PI * 180 + 90);
     }
+}
+
+std::string Plane::FloatToStringWithPrecision(float number, int precision) {
+    std::string result = std::to_string(number);
+    const size_t DOT = result.find('.');
+    
+    if (DOT != std::string::npos) {
+        size_t end_position = DOT + 1 + precision;
+        if (end_position < result.length()) {
+            result.erase(result.begin() + end_position, result.end());
+        }
+    }
+
+    return result;
 }
 
 } // namespace objects
